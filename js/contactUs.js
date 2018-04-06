bdbadb27558f9ffa7375043e2eded01d6aab9729
MariamAshrafFekry <mariamashraf096@gmail.com>
2018-04-06 23:45:30 +0200

    add google maps api

--- js/contactUs.js
+++ js/contactUs.js
@@ -1,8 +1,18 @@
- function myMap()
+jQuery.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDknmD-bIczC5pP5WPolW9zsx8xA8Ty6Cw",initMap);
+function initMap()
 {
-    var mapProp= {
-        center:new google.maps.LatLng(31.508742,30.120850),
-        zoom:5,
-};
-var map=new google.maps.Map(document.getElementById("idiv-map-location"),mapProp);
+        var latitude = 30.0594838;
+        var longitude = 31.2934839;
+        var LatLng={lat: latitude, lng: longitude};
+        var mapOptions=
+        {
+            center: LatLng,
+            zoom: 13,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("idiv-map-location"), mapOptions);
+        new google.maps.Marker({
+            position: LatLng,
+            map: map,
+        });
 }
