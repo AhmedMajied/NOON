a763575e2dfd6ccc8c18ad916bd79525b3680e15
Ahmed-Hussein-Karam <ahkcsit@gmail.com>
2018-04-07 18:37:39 +0200

    code documented

--- media-photos/js/media-photos.js
+++ media-photos/js/media-photos.js
@@ -1,5 +1,10 @@
+/* Author: Ahmed Hussein */
+
 $(document).ready(function () {
     $('.cspn-show-on-hover').hover(
+        /**
+         * handles the hover event on images to show/hide image date
+         */
         function(){
             $(this).find('.cfgr-show-on-hover').css('display','table-cell');
         },
--- media-videos/js/media-videos.js
+++ media-videos/js/media-videos.js
@@ -1,5 +1,10 @@
+/* Author: Ahmed Hussein */
+
 $(document).ready(function () {
     $('.cspn-show-on-hover').hover(
+        /**
+         * handles the hover event on videos to show/hide video duration
+         */   
         function(){
             $(this).find('.cfgr-show-on-hover').css('display','table-cell');
         },
