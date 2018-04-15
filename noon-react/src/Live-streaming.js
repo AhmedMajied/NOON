8b9187c6070b0d99f3c2e0befd70887104e176a0
ahmedMajeed <aabdelmeged70@yahoo.com>
2018-04-15 23:53:43 +0200

    initialization and routing paths

---
+++ noon-react/src/Live-streaming.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class LiveStreaming extends React.Component
+{
+    render(){
+        return(
+            <div>
+                Live Streaming
+                {/* call your components here */}
+            </div>
+        );
+    }
+}
