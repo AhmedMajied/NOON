307af9b480a6b0842a34e46ec28a35d84d2b52a7
Ahmed-Hussein-Karam <ahkcsit@gmail.com>
2018-05-03 12:43:27 +0200

    Media without screen

--- noon-react/src/Media-components/Media-card.js
+++ noon-react/src/Media-components/Media-card.js
@@ -1,46 +1,36 @@
 import React from 'react';
 
 class MediaCard extends React.Component {
-    constructor(props){
-		super();
-		this.state = {
-			metaCaption: props.date /*Either image date or video duration*/
-		};
-    }
-    
-    showVideoDuration = (e) => 
-        console.log('videoDuration')
-        /*var videoDuration = this.refs.video.duration;
-        console.log(videoDuration);
-        this.setState({metaCaption: videoDuration});*/
     
     render() {
-        var media;
-        
-        /*div shown on mouse hover*/
-        var hoverDiv = (
-            <div className="cdiv-show-on-hover">
-                <span className="cspn-show-on-hover">
-                    <figure className="cfgr-show-on-hover">
-                        <img src={this.props.hoverIconSrc} className="img img-responsive cimg-show-on-hover" alt=""/>
-                        <figcaption className="cfigcaption-show-on-hover">{this.state.metaCaption}</figcaption>
-                    </figure>
-                </span>
-            </div>
-        );
+        var media, metaCaption;
 
         if(this.props.sectionTitle === "Photos") {
+            metaCaption = this.props.date;
             media = (
                 <img src={this.props.mediaSrc} className="img img-responsive cmedia" alt="" />
             );
         }
         else {
+            metaCaption = "Video duration ...";
             media = (
                 <video className="cmedia" ref="video" onLoad={this.showVideoDuration} >
                     <source src={this.props.mediaSrc} />
                 </video>
             );
         }
+
+        /*div shown on mouse hover*/
+        var hoverDiv = (
+            <div className="cdiv-show-on-hover">
+                <span className="cspn-show-on-hover">
+                    <figure className="cfgr-show-on-hover">
+                        <img src={this.props.hoverIconSrc} className="img img-responsive cimg-show-on-hover" alt=""/>
+                        <figcaption className="cfigcaption-show-on-hover">{metaCaption}</figcaption>
+                    </figure>
+                </span>
+            </div>
+        );
 
         return (
             <div className="cdiv-media-card col-lg-3 col-md-4 col-sm-6">
