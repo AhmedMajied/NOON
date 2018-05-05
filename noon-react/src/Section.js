ae69996b147798fa2324fdd0d9caed0176db3422
ahmedMajeed <aabdelmeged70@yahoo.com>
2018-05-05 23:53:21 +0200

    remove video carousel from section page

--- noon-react/src/Section.js
+++ noon-react/src/Section.js
@@ -8,33 +8,14 @@ import {Sports} from "./Sports-component/Sports"
 export class Section extends React.Component
 {
 
-    constructor(){
-        super();
-        this.state = {
-            videosCarouselData: require("./fake-api/videosCarousel.json")
-        };
-    }
-
     render(){
-        var videosHeadlineStyle = {
-            width: '82px',
-            textAlign: 'center',
-            borderBottomLeftRadius: '50%',
-            borderBottomRightRadius: '50%'
-        };
-        var videosHeadlineTextStyle = {
-            verticalAlign: 'middle'
-        };
+        
 
         return(
             <div>
                 <Sports sportName="football"/>
                 <Photos/>
-                <VideosCarousel
-                    data={this.state.videosCarouselData}
-                    videosHeadlineStyle ={videosHeadlineStyle}
-                    videosHeadlineTextStyle = {videosHeadlineTextStyle}
-                />
+            
             </div>
         );
     }
